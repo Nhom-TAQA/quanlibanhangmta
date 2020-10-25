1e645e24cc56dc0c6ea8448b593e2a0c0fb91837
Sanganh <71274596+Sanganh@users.noreply.github.com>
2020-10-25 12:54:44 +0700

    sang anh

--- QL_BanHang/SuperMarket/BanHangForm.cs
+++ QL_BanHang/SuperMarket/BanHangForm.cs
@@ -16,11 +16,49 @@ namespace SuperMarket
         public BanHangForm()
         {
             InitializeComponent();
-            
+            LoadSP();
+            LoadTenSP();
+            Bang();
+            textBox8.Text = " Ngày " + DateTime.Now.Day + " tháng " + DateTime.Now.Month + " năm " + DateTime.Now.Year;
+            TienThua();
 
         }
 
-       
+        void TienThua()
+        {
+            int s;
+            if (textBox10.Text == "")
+            {
+                textBox9.Text = "";
+            }
+            else
+            {
+                if (textBox7.Text == "")
+                {
+                    textBox9.Text = "";
+                }
+                else
+                {
+                    s = int.Parse(textBox10.Text) - int.Parse(textBox7.Text);
+                    textBox9.Text = s.ToString();
+                }
+            }
+        }
+        BindingSource SanPham = new BindingSource();
+        void LoadSP()
+        {
+            string query = "select Ma as[Mã SP],Ten,DonVi as[Đơn vị],Gia as[Giá],SoLuong as[SL trong kho] from SanPham";
+            SanPham.DataSource = DataProvider.Instance.ExecuteQuery(query);
+        }
+
+        void LoadTenSP()
+        {
+            dataGridView1.DataSource = SanPham;
+            textBox1.DataBindings.Add(new Binding("text", dataGridView1.DataSource, "Ten", true, DataSourceUpdateMode.Never));
+        }
+        DataTable dt = new DataTable();
+
+
         private void button2_Click(object sender, EventArgs e)
         {
             
