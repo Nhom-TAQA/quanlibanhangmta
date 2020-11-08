ee07ff33d1ef8c124ce994ec7a5b6f664cb1f6d6
phousy <phousi4468@gmail.com>
2020-11-08 15:43:11 +0700

    /phousy

--- QL_BanHang/SuperMarket/LoginForm.cs
+++ QL_BanHang/SuperMarket/LoginForm.cs
@@ -18,9 +18,53 @@ namespace SuperMarket
             InitializeComponent();
         }
 
+        //
+        string PhanQuyen(string Ten)
+        {
+            string query = "SELECT ChucVu FROM dbo.TaiKhoan WHERE Ten = N'" + Ten + "'";
+            return DataProvider.Instance.DuLieu(query);
+        }
+
+        //
+        string TenDayDu(string Ten)
+        {
+            string query = "SELECT TenDayDu FROM dbo.TaiKhoan WHERE Ten = N'" + Ten + "'";
+            return DataProvider.Instance.DuLieu(query);
+        }
+
+        // Su kien dang nhap
         private void dangnhap_Click(object sender, EventArgs e)
         {
-            
+            string userName = tb_TenDN.Text;
+            string passWord = tb_MK.Text;
+
+            // Neu tai khoan
+            if (TaiKhoanDAO.Instance.Login(userName, passWord))
+            {
+
+                this.Hide();
+
+                if (PhanQuyen(userName) == "Quản lý")
+                {
+                    Admin f2 = new Admin();
+                    f2.ShowDialog();
+                }
+                this.Show();
+                if (PhanQuyen(userName) == "Quản kho")
+                {
+                }
+                if (PhanQuyen(userName) == "Bán hàng")
+                {
+                    BanHangForm bh = new BanHangForm();
+                    bh.ShowDialog();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Tên tài khoản hoặc mật khẩu chưa chính xác");
+            }
         }
+
+
     }
 }
